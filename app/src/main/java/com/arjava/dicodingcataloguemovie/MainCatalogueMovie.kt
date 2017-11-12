bb0bf700d7450e228024ad2b0545bab3e1341fa6
arjava <arjava379@gmail.com>
2017-11-12 17:54:10 +0300

    initial first commit

---
+++ app/src/main/java/com/arjava/dicodingcataloguemovie/MainCatalogueMovie.kt
@@ -0,0 +1,12 @@
+package com.arjava.dicodingcataloguemovie
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainCatalogueMovie : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_catalogue_movie)
+    }
+}
